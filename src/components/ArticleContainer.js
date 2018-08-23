faf2a7997604803e884c0b6cf44b9c73e921cfe2
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-08-22 22:55:24 -0400

    appears to be working ok

--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ArticleItem from './ArticleItem';
 
-const ArticleContainer = ({ articles, readLater, handleSaveArticleToReadLater, handleReadArticle }) => (
-    <div className="wire-container">
-          <h2 className="wire-container-header">Latest Articles from the New York Times</h2>
+class ArticleContainer extends React.Component {
 
-        {articles.map((a, index) => {
-            return (
-              <ArticleItem article={a} key={index} id={parseInt(index)} handleSaveArticleToReadLater={handleSaveArticleToReadLater} handleReadArticle={handleReadArticle} articles={articles} readLater={readLater}/>
-            );
+  render() {
+    return (
+
+      <div className="wire-container">
+        <h2 className="wire-container-header">Latest Articles from the New York Times</h2>
+
+        {this.props.articles.map((a, index) => {
+          return (
+            <ArticleItem article={a} key={index} id={parseInt(index)} handleSaveArticleToReadLater={this.props.handleSaveArticleToReadLater} handleReadArticle={this.props.handleReadArticle} articles={this.props.articles} readLater={this.props.readLater}/>
+          );
         })
       }
 
     </div>
-  );
+  )
+}
+}
+
 
 export default ArticleContainer;
